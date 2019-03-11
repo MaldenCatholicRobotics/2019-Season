1097b17ff632298a1b6432423deb952f7a28d772
Harrison Jumper <39711205+JumpmanMC@users.noreply.github.com>
2019-03-10 21:05:57 -0400

    Update main.c
    Reworked the fire_scan to include movement to increase the margin of error

--- main.c
+++ main.c
@@ -1,20 +1,27 @@
 #include <kipr/botball.h>
 #include <stdbool.h>
-void line_follower(int runtime, int m_port_l, int m_port_r, int ir_port_l, int ir_port_r, int slow_speed, int reg_speed, int fast_speed, int tape_benchmark);
+void line_follower(int runtime, int slow_speed, int reg_speed, int fast_speed, int tape_benchmark);
 bool fire_scan(int red_channel, double red_benchmark);
 
 int main()
 {
-    line_follower(100, 0, 1, 1, 0, 800, 1200, 1600, 3000);
+    //creates global variables to hold the ports of the motors and ir sensors
+    int m_port_l = 0;
+    int m_port_r = 1;
+    int ir_port_l = 1;
+    int ir_port_r = 0;
+    
+    
     fire_scan(0, 0.1);
+    //change runtime to figure out 
+    line_follower(100, 800, 1200, 1600, 3000);
+    
     return 0;
 }
 //runtime- the number of tenths of seconds the bot should follow the line
-//m_port_l and m_port_r- the ports of the left and right motors
-//ir_port_l and ir_port_r- the ports of the left and right ir sensors
 //slow_speed, reg_speed and fast_speed- the motor speeds for going straight and turning
 //tape_benchmark- the ir value that decides if the sensor is on the tape or not
-void line_follower(int runtime, int m_port_l, int m_port_r, int ir_port_l, int ir_port_r, int slow_speed, int reg_speed, int fast_speed, int tape_benchmark)
+void line_follower(int runtime, int slow_speed, int reg_speed, int fast_speed, int tape_benchmark)
 {
     //creates a temporary counter to control the while loop
     int counter = 0;
@@ -45,6 +52,7 @@ void line_follower(int runtime, int m_port_l, int m_port_r, int ir_port_l, int i
             //printf("analog left: %i /n", analog(0));
             //printf("analog right: %i /n", analog(1));
         }
+        
         //assumed that the right sensor is off tape but the left is on
        else
         {
@@ -68,8 +76,8 @@ void line_follower(int runtime, int m_port_l, int m_port_r, int ir_port_l, int i
 bool fire_scan(int red_channel, double red_benchmark) 
 { 
     //create a counter variable for the while loop
-    int counter = 0; 
-
+    int counter_1 = 0; 
+    
     //creates variables to store values gathered by camera
     double red_confidence; 
     double red_total; 
@@ -89,25 +97,34 @@ bool fire_scan(int red_channel, double red_benchmark)
     printf("%d\n", get_object_count(0));    
     msleep(100); 
 
-
     //one snapshot and about one tenth of a second per iteration
-    while(counter < 50) 
-
+    while(counter_1 < 5) 
     { 
+         int counter_2 = 0;
+        while(counter_2 < 10)
+        {
+            //creates a counter variable
+           
+            camera_update(); 
 
-        camera_update(); 
+            //gets the confidence that the building is on fire
+            red_confidence = get_object_confidence(red_channel, 0);
 
-        //gets the confidence that the building is on fire
-        red_confidence = get_object_confidence(red_channel, 0);
+            //print statement for debugging
+            //printf("red_confidence: %f\n", red_confidence);
+
+            //adds all snapshot confidences together
+            red_total += red_confidence; 
+            msleep(100); 
+            counter_2++; 
+        }
         
-        //print statement for debugging
-        //printf("red_confidence: %f\n", red_confidence);
-
-        //adds all snapshit confidences together
-        red_total += red_confidence; 
-        msleep(100); 
-        counter++; 
-
+        //drives the bot forward very slowly for one tenth of a second
+        //this allows a larger margin of error for the camera
+        mav(m_port_l, 100);
+        mav(m_port_r, 100);
+        msleep(100);
+        counter_1++;
     } 
 
     red_average = red_total/50; 
@@ -119,21 +136,16 @@ bool fire_scan(int red_channel, double red_benchmark)
 
     //if building is on fire
     if (red_average >= red_benchmark) 
-
     { 
-
         printf("FIRE\n");
         return true; 
-
     } 
 
     //if building is not on fire
     else 
-
     { 
         printf("SAFE\n");
         return false; 
-
     } 
     return 0;
 } 
