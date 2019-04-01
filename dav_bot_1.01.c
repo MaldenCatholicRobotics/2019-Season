4bb11a9a08577f68583d4825452aef636646028d
MaldenCatholicRobotics <48227455+MaldenCatholicRobotics@users.noreply.github.com>
2019-04-01 14:20:24 -0400

    Update dav_bot_1.01.c

--- dav_bot_1.01.c
+++ dav_bot_1.01.c
@@ -7,7 +7,7 @@ void line_follower();
 
 void reverse_line_follower();
 
-void fire_scan();
+bool fire_scan(double red_benchmark);
 
 void drive();
 
@@ -38,8 +38,11 @@ int mid_up = 50;
 int claw_close = 80;
 int claw_open = 80;
 
-int main()
+//variables for camera use
+int red_benchmark = 0.2;
+int numpics = 30;
 
+int main()
 {
 	enable_servos();
 	set_servo_position(base_port, base_down);
@@ -66,6 +69,80 @@ void reverse_line_follower()
 	
 }
 
+//determines if a building or medical center is on fire and returns a boolean value
+//precondition: the camera is pointed at the building
+//red_benchmark- the minimum average confidence for the building to be on fire
+bool fire_scan(double red_benchmark) 
+{ 
+    //create a counter variable for the while loop
+    int counter_1 = 0; 
+    int counter_2 = 0;
+    //creates variables to store values gathered by camera
+    double red_confidence; 
+    double red_total; 
+    double red_average; 
+
+    //configures and updates camera
+    camera_open_black(); 
+    camera_load_config("fire"); 
+    camera_update(); 
+    //prints number of channels
+    printf("%i\n", get_channel_count()); 
+    msleep(100); 
+    camera_update(); 
+
+    //prints number of objects in channel
+    printf("%d\n", get_object_count(0));    
+    msleep(100); 
+
+    //one picture and about one tenth of a second per iteration
+    //creates a buffer period for the camera to set up
+    while(counter_1 < 10) 
+    { 
+           //creates a counter variable
+           camera_update();
+        printf("huh");
+	    msleep(100);
+	    counter_1++;
+    }   
+      //creates a running tally of the confidence in each snapshot
+    while(counter_2 < num_pics)
+    {
+            camera_update(); 
+
+            //gets the confidence that the building is on fire
+            red_confidence = get_object_confidence(red_channel, 0);
+
+            //print statement for debugging
+            //printf("red_confidence: %f\n", red_confidence);
+
+            //adds all snapshot confidences together
+            red_total += red_confidence; 
+            msleep(100); 
+            counter_2++; 
+      }
+        
+    red_average = red_total/num_pics; 
+    camera_update(); 
+    camera_close(); 
+
+    //print statement for debugging
+    printf("red average:%f\n", red_average);   
+    
+    //if building is on fire
+    if (red_average >= red_benchmark) 
+
+    { 
+        printf("FIRE\n");
+        return true; 
+    } 
+    //if building is not on fire
+    else 
+    { 
+        printf("SAFE\n");
+        return false; 
+    } 
+} 
 
 
 //drives create at an input speed for a given time
