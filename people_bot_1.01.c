b6bffaf79ed51317d2e7b5df3a7fdb4a4baabb1d
MaldenCatholicRobotics <48227455+MaldenCatholicRobotics@users.noreply.github.com>
2019-04-02 08:07:54 -0400

    Update people_bot_1.01.c

--- people_bot_1.01.c
+++ people_bot_1.01.c
@@ -5,6 +5,40 @@
 //
 
 
+//FUNCTION DECLARATIONS
+void line_follower(int runtime, int tape_benchmark);
+
+void reverse_line_follower(int runtime, int tape_benchmark);
+
+bool fire_scan(double red_benchmark);
+
+void servo_change(int op, int wp, int servoport, int speed);
+
+void turn(int time, int power, int port) ;
+
+void drive(int time, int l_power, int r_power); 
+
+void drive_until_line(int tape_benchmark, int l_power, int r_power);
+
+void scan_centers();
+
+void collect_people();
+
+void deliver_people();
+
+void centers_to_buildings();
+
+void buildings_to_centers();
+
+void opening_sequence();
+
+void first_row();
+
+void second_row();
+
+void third_row();
+
+void fourth_row();
 
 int main()
 {
