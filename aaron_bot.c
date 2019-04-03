6f39d5bea97304ad7eb7c9c1d0118d8c3da0fc85
MaldenCatholicRobotics <48227455+MaldenCatholicRobotics@users.noreply.github.com>
2019-04-03 15:38:22 -0400

    Update aaron_bot.c

--- aaron_bot.c
+++ aaron_bot.c
@@ -46,3 +46,45 @@ int main()
   fourth_row();
   return 0;
 }
+
+//moves a servo to a desired position at a certain speed
+//op- the starting position of the servo
+//wp- the end positon of the servo
+//servoport- the port of the servo 
+//speed- the increment by which the servo position is repeatedly moved 
+void servo_change(int op, int wp, int servoport, int speed) 
+{   
+    if (op > wp) 
+    { 
+        while(op > wp)  
+        {   
+        //increments the position
+        op -= speed;  
+            
+        //sets the servo position to the incremented position
+        set_servo_position(servoport, op);   
+        
+        //to debug issues with the code 
+        //printf("opening or closing claw/"); 
+
+        msleep(100); 
+
+        }   
+    }
+    else 
+    { 
+        while(wp > op)  
+        {   
+            //increments the position
+            op += speed; 
+
+            //sets the servo position to the incremented position
+            set_servo_position(servoport, op);   
+
+            //to debug issues with the code 
+            //printf("opening or closing claw/");//to debug issues with the code  
+
+            msleep(100); 
+         }   
+    } 
+}    
