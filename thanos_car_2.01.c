1a15a050f7795654bdeea343ceef135c9e5ef99d
MaldenCatholicRobotics <48227455+MaldenCatholicRobotics@users.noreply.github.com>
2019-04-05 15:47:57 -0400

    Update thanos_car_2.01.c

--- thanos_car_2.01.c
+++ thanos_car_2.01.c
@@ -1,7 +1,19 @@
-//creates global variables to hold the ports of the servos, motors and ir sensors
+#include <kipr/botball.h>
+#include <stdbool.h>
+
+//Notes
+//TOOK OUT CLAW_SLIGHTLY_OPEN AND ARM_VERY_UP
+
+//FUNCTION DECLARATIONS
+void line_follower(int runtime, int tape_benchmark);
+
+void reverse_line_follower(int runtime, int tape_benchmark);
+
+//GLOBAL VARIABLES
+//ports of the servos, motors and ir sensors
 int m_port_l = 0;
 int m_port_r = 1;
-int ir_port = 0;
+int f_ir_port = 0;
 int r_ir_port = 1; 
 int servo_port_arm = 0;
 int servo_port_claw = 2;
@@ -16,16 +28,18 @@ int red_channel = 0;
 //the number of pictures to be taken in the fire_scan function
 int num_pics = 20;
 
-//holds the benchmarks for the camera
+//ir value of the black tape
 int black_tape = 3000;
+
+//the average confidence the camera needs to determine a building is on fire
 double fire_benchmark = 0.2;
 
-//speeds for the line follower
+//speeds for the bot to drive forwards
 int reg_speed = 1200;
 int fast_speed = 1600;
 int slow_speed = 800;
 
-//speeds for the reverse line follower
+//speeds for the bot to drive backwards
 int r_reg_speed = (0-1200);
 int r_fast_speed = (0-1600);
 int r_slow_speed = (0-800);
@@ -35,15 +49,15 @@ int claw_open = 1400;
 int claw_close = 550;
 int arm_up = 800;
 int arm_down = 250;
-int claw_slightly_open = 1200;
-int arm_very_high = 1100;
 
-//variables to store data collected by the bot
+//data collected by scan_buildings function
 int building_on_fire;
-int center_on_fire;
-int safe_center;
 int safe_building_right;
 int safe_building_left;
+
+//data collected by scan_centers function
+int center_on_fire;
+int safe_center;
 
 //MAIN
 int main()
@@ -53,9 +67,44 @@ int main()
     //sets inital claw position to up and closed	  
     set_servo_position(servo_port_arm, arm_up);
     set_servo_position(servo_port_claw, claw_close);
-    opening_sequence();
-	scan_buildings();
-    deliver_ground();
     disable_servos();
     return 0;
+}
+
+//FUNCTIONS
+
+//follows the edge of a line using a single ir sensor
+//runtime- the number of tenths of seconds the bot should follow the line
+//tape_benchmark- the ir value that decides if the sensor is on the tape or not
+void line_follower(int runtime, int tape_benchmark)
+{
+    //creates a temporary counter to control the while loop
+    int counter = 0;
+    //keeps the program running for a specified time
+    //runtime is multiplied by ten to account for 1/100 second iterations but 1/10 second inputs
+    while(counter <= 10*runtime)
+    {
+        //if sensor on tape
+        if(analog(f_ir_port) >= 3000)
+        {
+            //veer slightly left
+            mav(m_port_l, 1200);
+            mav(m_port_r, 1000);
+	    //checks position every 1/100 seconds
+            msleep(10);
+        }
+        //assumed that sensor is off tape
+        else
+        {
+            //veer slightly right
+            mav(m_port_l, 1000);
+            mav(m_port_r, 1200);
+            //checks position every 1/100 seconds
+            msleep(10);
+        }
+	//increments counter
+        counter++;
+    }
+    //turns off motors
+    ao();
 }
