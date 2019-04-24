f702181e324ac2513929a97fe7273015dfa4eb7f
MaldenCatholicRobotics <48227455+MaldenCatholicRobotics@users.noreply.github.com>
2019-04-24 14:54:48 -0400

    Update create_line_follower.c

--- create_line_follower.c
+++ create_line_follower.c
@@ -5,7 +5,7 @@ void line_follower(int runtime, int tape_value, int speed)
 int main()
 
 {
-    line_follower(10, black_tape);
+    line_follower(10, black_tape, 1500);
     return 0;
 }
 
@@ -17,12 +17,19 @@ void line_follower(int runtime, int tape_value, int speed)
     int lcliffamt = get_create_lcliff_amt();
     int rcliffamt = get_create_rcliff_amt();
     
-    if(lcliffamt >  tape_value && rcliffamt > tape_value)
+    if(lcliffamt <  tape_value && rcliffamt < tape_value)
     {
       create_drive_connect(speed, speed);
     }
-    if(lcliffamt >  tape_value && rcliffamt > tape_value)
+    else if(lcliffamt <  tape_value && rcliffamt > tape_value)
     {
-      create_drive_connect(speed, speed);
+      create_drive_connect(speed/2, speed);
     }
-    
+    else 
+    {
+      create_drive_connect(speed, speed/2);
+    }
+    msleep(10);
+    counter += 10;
+  }
+}
