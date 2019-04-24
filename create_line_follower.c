f2fb69963ac94f46e09c8c57c47c29455e6ef716
MaldenCatholicRobotics <48227455+MaldenCatholicRobotics@users.noreply.github.com>
2019-04-24 14:52:37 -0400

    Update create_line_follower.c

--- create_line_follower.c
+++ create_line_follower.c
@@ -1,1 +1,28 @@
+#include <kipr/botball.h>
 
+void line_follower(int runtime, int tape_value, int speed)
+
+int main()
+
+{
+    line_follower(10, black_tape);
+    return 0;
+}
+
+void line_follower(int runtime, int tape_value, int speed)
+{
+  int counter = 0;
+  while(runtime < counter)
+  {
+    int lcliffamt = get_create_lcliff_amt();
+    int rcliffamt = get_create_rcliff_amt();
+    
+    if(lcliffamt >  tape_value && rcliffamt > tape_value)
+    {
+      create_drive_connect(speed, speed);
+    }
+    if(lcliffamt >  tape_value && rcliffamt > tape_value)
+    {
+      create_drive_connect(speed, speed);
+    }
+    
